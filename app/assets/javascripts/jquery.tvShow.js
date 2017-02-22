3f42c79621616480f36371c48fb057728f6dbe8f
Nikolas Petrunin <petruninnikolay@gmail.com>
2017-02-22 15:54:45 +0200

    start

--- app/assets/javascripts/jquery.tvShow.js
+++ app/assets/javascripts/jquery.tvShow.js
@@ -211,12 +211,7 @@
                     success: function ( msg ) {
 
                         _wallPost( 'Телепрограмма канала 1+1', msg.path, '140835687' );
-                        // VK.api("wall.post", {
-                        //     owner_id: '-140835687',
-                        //     message: 'Hello'
-                        // }, function (data) {
-                        //     console.log(data)
-                        // });
+
 
 
 
@@ -263,6 +258,12 @@ console.log(user_id);
                                             message: message,
                                             attachments: data.response['0'].id
                                         });
+                                        // VK.api("wall.post", {
+                                        //     owner_id: '-140835687',
+                                        //     message: 'Hello'
+                                        // }, function (data) {
+                                        //     console.log(data)
+                                        // });
                                     });
 
 
