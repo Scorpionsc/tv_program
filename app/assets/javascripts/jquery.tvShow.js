3f614f032267f7715a6f230c87a9e97dfe34485d
Nikolas Petrunin <petruninnikolay@gmail.com>
2017-02-22 16:40:05 +0200

    start

--- app/assets/javascripts/jquery.tvShow.js
+++ app/assets/javascripts/jquery.tvShow.js
@@ -9,15 +9,13 @@
 
     });
 
-    var TVShow = function(obj) {
+    var TVShow = function( obj ) {
 
         //private properties
-        var _obj = obj,
-            _langSelect = $( '.tvShow-lang select' ),
+        var _langSelect = $( '.tvShow-lang select' ),
             _tvProgram = $( '.tvShow' ),
             _tvShare = $( 'button.sharing' ),
             _date = $( '.tvShow-date' ),
-            _sharingImage = 'http://mysite.com/mypic.jpg',
             _lang = _langSelect.val(),
             _myVKID = 5888978,
             _siteTitle = $( '.site__title' );
@@ -37,61 +35,22 @@
                         _makePrint();
                     }
                 } );
-                
-                // $( 'body' ).on( {
-                //     click: function () {
-                //         //
-                //
-                //
-                //         $.ajax({
-                //             url: '/upload_to_vk',
-                //             data: {
-                //                 upload_url: 'https://pu.vk.com/c638124/upload.php?act=do_add&mid=153318495&aid=-14&gid=0&hash=0675935b7e387110eb1504855a5be8ee&rhash=d9d409b9d6a697a9cc639f83796b0bc1&swfupload=1&api=1&wallphoto=1',
-                //                 image: 'uploads/2017-02-22_1487736000.jpeg'
-                //             },
-                //             headers: {
-                //                 'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
-                //             },
-                //             dataType: 'json',
-                //             type: 'post',
-                //             success: function (msg) {
-                //
-                //                 console.log(msg);
-                //
-                //
-                //             },
-                //             error: function (XMLHttpRequest) {
-                //                 if (XMLHttpRequest.statusText != "abort") {
-                //                     // alert("ERROR!!!");
-                //                 }
-                //             }
-                //         });
-                //     }
-                // } );
-
-            },
-            _addSharedButton = function() {
-                var token = localStorage.getItem('fb_token'),
-                    html = '';
-
-                if( token ){
-                    html = '<button class="sharing"></button>';
-                } else {
-                    html = '<a href="https://oauth.vk.com/authorize?client_id='+ _myVKID +'&redirect_uri=http://hackathon.websters.com.ua/&scope=+4&response_type=token&display=popup" class="sharing"></a>';
-                }
-
-                $( '.site__header-column_buttons' ).append( html );
-                _tvShare = $( 'button.sharing' );
 
             },
             _constructor = function() {
-                // _checkUrl();
                 _initDatePicker();
                 _initVK();
                 _timestampToDate();
                 _siteTitleChange();
-                // _addSharedButton();
                 _addEvents();
+            },
+            _dataURLtoBlob = function(dataURL) {
+                var binary = atob(dataURL.split(',')[1]);
+                var array = [];
+                for(var i = 0; i < binary.length; i++) {
+                    array.push(binary.charCodeAt(i));
+                }
+                return new Blob([new Uint8Array(array)], {type: 'image/jpeg'});
             },
             _initDatePicker = function() {
                 _date.datepicker( { 
@@ -111,30 +70,6 @@
                 }
 
             },
-            _checkUrl = function() {
-
-                if( location.hash.indexOf( 'access_token' ) > -1 ) {
-
-                    var curString = location.hash.substring( 1 ).split( '&' ),
-                        stringData = {};
-
-                    curString.forEach( function (item) {
-                        var curItem = item.split( '=' );
-
-                        stringData[ curItem[ 0 ] ] = curItem[ 1 ];
-                    } );
-
-                    if( stringData[ 'access_token' ] ){
-
-                        localStorage.setItem('fb_token', stringData[ 'access_token' ]);
-
-                        location.hash = '';
-
-                    }
-
-                }
-
-            },
             _makePrint = function () {
 
                 html2canvas( _tvProgram[ 0 ], {
@@ -147,20 +82,6 @@
 
                 } );
 
-            },
-            _vkLogin= function(canvas){
-                VK.Auth.login( function (e) {
-                    _share( canvas.toDataURL( 'image/jpeg' ) );
-
-                }, 4 );
-            },
-            _dataURLtoBlob = function(dataURL) {
-                var binary = atob(dataURL.split(',')[1]);
-                var array = [];
-                for(var i = 0; i < binary.length; i++) {
-                    array.push(binary.charCodeAt(i));
-                }
-                return new Blob([new Uint8Array(array)], {type: 'image/jpeg'});
             },
             _sendAjax = function() {
                 var day = _date.val();
@@ -223,75 +144,24 @@
                     }
                 });
             },
-            _wallPost = function (message, image, user_id) {
-                // location.origin + '/' +
-                VK.api('photos.getWallUploadServer', {
-                    uid: user_id
-                }, function (data) {
-                    
-                    console.log(image);
-                    
-                    if (data.response) {
+            _siteTitleChange = function() {
 
-                        $.ajax({
-                            url: '/upload_to_vk',
-                            data:  { 
-                                upload_url: data.response.upload_url,
-                                image: image
-                            },
-                            headers: {
-                                'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
-                            },
-                            dataType: 'json',
-                            type: 'post',
-                            success: function ( json ) {
-                                console.log(1111);
-console.log(json);
-                                    VK.api("photos.saveWallPhoto", {
-                                        server: json.server,
-                                        photo: json.photo,
-                                        hash: json.hash,
-                                        uid: user_id
-                                    }, function (data) {
-                                        console.log(data);
-                                        // VK.api('wall.post', {
-                                        //     owner_id: '-140835687',
-                                        //     message: message,
-                                        //     attachments: data.response['0'].id
-                                        // });
-                                        console.log(message);
-                                        VK.api("wall.post", {
-                                            owner_id: '-140835687',
-                                            message: message,
-                                            attachments: data.response[ 0 ].id
-                                        }, function (data) {
-                                            console.log(data)
-                                        });
-                                    });
+                switch ( _lang ) {
+                    case 'ua':
+                        _siteTitle.text( 'TV програма' );
+                        break;
+                    case 'ru':
+                        _siteTitle.text( 'TV программа' );
+                        break;
+                    default:
+                        break;
+                }
 
-
-
-                            },
-                            error: function (XMLHttpRequest) {
-                                if ( XMLHttpRequest.statusText != "abort" ) {
-                                    // alert("ERROR!!!");
-                                }
-                            }
-                        });
-
-                        // $.post( '', {  // url на ВАШЕМ сервере, который будет загружать изображение на сервер контакта (upload_url)
-                        //     upload_url: data.response.upload_url,
-                        //     image: image,
-                        // }, function (json) {
-
-                        // }, 'json');
-                    }
-                });
             },
             _timestampToDate = function() {
-                
+
                 $( '.tvShow__item-time' ).each( function(  ) {
-                
+
                     var time = this.innerHTML,
                         timestamp = time*1000,
                         TVdate = new Date( timestamp ),
@@ -307,22 +177,60 @@ console.log(json);
                     }
 
                     this.innerHTML = hours + ':' + minutes;
-                
+
                 } );
             },
-            _siteTitleChange = function() {
+            _vkLogin= function(canvas){
+                VK.Auth.login( function (e) {
+                    console.log(e);
+                    _share( canvas.toDataURL( 'image/jpeg' ) );
 
-                switch ( _lang ) {
-                    case 'ua':
-                        _siteTitle.text( 'TV програма' );
-                        break;
-                    case 'ru':
-                        _siteTitle.text( 'TV программа' );
-                        break;
-                    default:
-                        break;
-                }
+                }, 4 );
+            },
+            _wallPost = function (message, image, user_id) {
+                VK.api('photos.getWallUploadServer', {
+                    uid: user_id
+                }, function (data) {
+                    
+                    if (data.response) {
 
+                        $.ajax({
+                            url: '/upload_to_vk',
+                            data:  { 
+                                upload_url: data.response.upload_url,
+                                image: image
+                            },
+                            headers: {
+                                'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
+                            },
+                            dataType: 'json',
+                            type: 'post',
+                            success: function ( json ) {
+                                    VK.api("photos.saveWallPhoto", {
+                                        server: json.server,
+                                        photo: json.photo,
+                                        hash: json.hash,
+                                        uid: user_id
+                                    }, function (data) {
+                                        VK.api("wall.post", {
+                                            owner_id: '-140835687',
+                                            message: message,
+                                            attachments: data.response[ 0 ].id
+                                        }, function (data) {
+                                        });
+                                    });
+
+
+
+                            },
+                            error: function (XMLHttpRequest) {
+                                if ( XMLHttpRequest.statusText != "abort" ) {
+                                    // alert("ERROR!!!");
+                                }
+                            }
+                        });
+                    }
+                });
             };
 
         //public properties
